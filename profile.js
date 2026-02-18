2499b61d8339827826668f51c3e32a03667bd6a8
zaferavcilari-svg <zaferavcilari@gmail.com>
2026-02-18 22:26:57 +0300

    Delete profile.js

--- profile.js
+++
@@ -1,9 +0,0 @@
-const profileBtn = document.getElementById("profileBtn");
-const profilePanel = document.getElementById("profilePanel");
-
-profileBtn.onclick = () => {
-  profilePanel.style.display =
-    profilePanel.style.display === "block" ? "none" : "block";
-};
-
-profilePanel.innerHTML = "<h3>Profil</h3><p>Demo sürüm</p>";
