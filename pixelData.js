8ce586338b6a78adef32ca4882aa366cf941dd36
zaferavcilari-svg <zaferavcilari@gmail.com>
2026-03-15 13:59:57 +0300

    pixelData.js
    Updated localStorage key and console log message in sync function.

--- pixelData.js
+++ pixelData.js
@@ -1,18 +1,16 @@
 export const pixelData = {
     selectedColor: '#000000',
     canvasSize: 128,
-    // Palet renkleri
     colors: [
         '#000000', '#808080', '#FFFFFF', '#FF0000', '#800000', 
         '#FFA500', '#FFDBAC', '#FFFF00', '#006400', '#008000', 
         '#0000FF', '#00008B', '#800080', '#FFC0CB', '#4B0082', '#00FFFF'
     ],
-    // Senkronizasyon (1 saniyede bir çalışacak tetikleyici)
     sync: function(canvasElement) {
         setInterval(() => {
             const data = canvasElement.toDataURL();
-            localStorage.setItem('artCanvas_backup', data);
-            console.log("Art Canvas: 1s Senkronizasyon sağlandı.");
+            localStorage.setItem('artCanvas_save', data);
+            console.log("Senkronize edildi...");
         }, 1000);
     }
 };
